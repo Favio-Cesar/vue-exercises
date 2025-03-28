dfbf36d50845bd44cb57e36e1e202ca519b10928
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-03-28 11:34:54 +0100

    Ejercicio 1

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './assets/reset.css'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')

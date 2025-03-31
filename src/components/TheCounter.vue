bc8abb6974a328ea26ffcdf5bc47eae15c746ca7
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-03-31 11:44:53 +0200

    Ejercicio 14 Vue Watchers

--- src/components/TheCounter.vue
+++ src/components/TheCounter.vue
@@ -1,15 +1,27 @@
 <script lang="ts" setup>
 import { useCounter } from '@/composables/TheCounter'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 const { addValue, subtractValue, canAdd, canSubtract, count } = useCounter()
 const multiplicator = ref(2)
 const multiplication = computed(() => multiplicator.value * count.value)
+const message = ref('Add or subtract to see the state of your value')
+watch(count, async () => {
+  if (count.value < 10 && count.value > 0) {
+    message.value = 'You are inside the adecuate parameters'
+  }
+  if (count.value == 10) {
+    message.value = 'You are in the max value'
+  }
+  if (count.value == 0) {
+    message.value = 'You are in the min value'
+  }
+})
 </script>
 
 <template>
   <button v-if="canAdd" class="button__increase" @click="addValue">Increment</button>
   <button v-if="canSubtract" class="button__decrease" @click="subtractValue">Decrement</button>
-
+  <h1>{{ message }}</h1>
   <p :class="{ p__medium: true, 'p__counter--green': !canAdd }">
     {{ count }}
   </p>
